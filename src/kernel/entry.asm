49ec857a25bb76e931c36fbebb7a927b13d35d00
armanfarhangi <armanfdev@gmail.com>
2017-10-30 13:43:38 -0600

    Added barebones kernel

--- src/kernel/entry.asm
+++ src/kernel/entry.asm
@@ -1,6 +1,7 @@
 [bits 32]
 
 ; includes an unresolved reference to a label called "main", which is the core kernel function
+; this is necessary because sometimes the C compiler will not place main as the first function
 [extern main]
 
 call main
